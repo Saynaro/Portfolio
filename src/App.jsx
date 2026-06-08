8483fa81377bd36b19b270e9a75c753a525e1dca
Khalid Sainaro <khalid.cizdoev777@gmail.com>
2026-06-08 18:32:24 +0200

    implement smooth scrolling for anchor links using Lenis library

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,30 @@ function App() {
 
     requestAnimationFrame(raf);
 
-    return () => lenis.destroy();
+    const handleAnchorClick = (e) => {
+      const targetLink = e.target.closest('a');
+      if (targetLink) {
+        const href = targetLink.getAttribute('href');
+        if (href && href.startsWith('#')) {
+          e.preventDefault();
+          const targetEl = document.querySelector(href);
+          if (targetEl) {
+            lenis.scrollTo(targetEl, {
+              offset: 0,
+              duration: 1.5,
+              easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+            });
+          }
+        }
+      }
+    };
+
+    document.addEventListener('click', handleAnchorClick);
+
+    return () => {
+      document.removeEventListener('click', handleAnchorClick);
+      lenis.destroy();
+    };
   }, []);
 
   return (
